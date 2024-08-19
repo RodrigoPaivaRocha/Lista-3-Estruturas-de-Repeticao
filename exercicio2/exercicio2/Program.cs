4ee700eeff10f563f69fbfd35e6d2bf6b7325b5e
Rodrigo Paiva Rocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2024-08-19 19:24:03 -0300

    Add files via upload

---
+++ exercicio2/exercicio2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int primeiroValor, segundoValor;
+
+            Console.Write("Digite o primeiro valor: ");
+            primeiroValor = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.Write("Digite o segundo valor (deve ser maior que o primeiro): ");
+                segundoValor = int.Parse(Console.ReadLine());
+            } while (segundoValor <= primeiroValor);
+
+            Console.WriteLine($"Primeiro valor: {primeiroValor}, Segundo valor: {segundoValor}");
+
+            Console.ReadKey();
+        }
+    }
+}
