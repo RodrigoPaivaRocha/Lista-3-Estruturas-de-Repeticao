5b0254350835eecfcfd3475c6066901d7fab5eab
Rodrigo Paiva Rocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2024-08-19 19:24:58 -0300

    Add files via upload

---
+++ exercicio4/exercicio4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 5;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{numero} x {i} = {numero * i}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
