296da5fc29559cf4ab25bee006828ad71f82d917
Rodrigo Paiva Rocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2024-08-19 19:24:29 -0300

    Add files via upload

---
+++ exercicio3/exercicio3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char sexo;
+
+            do
+            {
+                Console.Write("Digite o sexo (F/M): ");
+                sexo = char.ToUpper(Console.ReadLine()[0]);
+            } while (sexo != 'F' && sexo != 'M');
+
+            Console.WriteLine($"Sexo digitado: {sexo}");
+
+            Console.ReadKey();
+        }
+    }
+}
