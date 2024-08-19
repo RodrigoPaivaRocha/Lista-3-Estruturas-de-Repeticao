9012736bfccfdf0024a3baa8d5b05d9061d24d74
Rodrigo Paiva Rocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2024-08-19 19:23:09 -0300

    Add files via upload

---
+++ exercicio1/exercicio1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+
+            do
+            {
+                Console.Write("Digite um valor positivo: ");
+                valor = int.Parse(Console.ReadLine());
+            } while (valor <= 0);
+
+            Console.WriteLine($"Você digitou um valor positivo: {valor}");
+
+            Console.ReadKey();
+        }
+    }
+}
