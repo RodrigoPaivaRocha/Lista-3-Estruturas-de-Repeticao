0792589e59905a0096d4cf24c68289e13cc9bd30
Rodrigo Paiva Rocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2024-08-19 19:26:15 -0300

    Add files via upload

---
+++ exercicio7/exercicio7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine($"Tabuada do {i}:");
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine($"{i} x {j} = {i * j}");
+                }
+                Console.WriteLine(); // Adicionar uma linha em branco entre as tabuadas
+            }
+
+            Console.WriteLine("Pressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
